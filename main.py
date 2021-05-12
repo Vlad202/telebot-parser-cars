83ac58c1dc6ce8205b88525e6c81152a6f88d5a6
Vlad202 <vladandum08@gmail.com>
2021-05-12 16:43:30 +0300

    (beta) split the auction of the second site into different archives

--- main.py
+++ main.py
@@ -65,23 +65,23 @@ def main():
 			registration = f"Дата первой регистрации:   {table_rows[5].find_all('td')[-1].text.strip()}"
 			auction_date = f"Дата аукциона:   {post_details_soup.find('div', {'id': 'auction_bid'}).find('div', {'class': 'top'}).find_all('dd')[-1].text.strip()}"
 			text = f"{header}\n\n{mileage}\n{engine}\n{transmission}\n{registration}\n{auction_date}"
-			image_url = URL + post.find('img').attrs['src']
-			image = requests.get(URL + post_details_url).content
+			images_url = post.find_all('a', {'class': 'thumbnail'})
 			os.mkdir(header)
-			with open(f'./{header}/'+header+'.png', 'wb') as f:
-				f.write(image)
+			for url in images_url:
+				image = requests.get(URL + url.attrs['href']).content
+				with open(f'./{header}/'+header+'.png', 'wb') as f:
+					f.write(image)
 			with open(f'./{header}/'+header+'.txt', 'w') as f:
 				f.write(text)
 			shutil.make_archive(header, 'zip', header)
-			shutil.rmtree(f'{header}/')
-			# with open(header+'.zip', 'rb') as zipObj:
 			for chat_id in ids_list:
 				try:
 					app.send_document(chat_id, open(header+'.zip','rb'))
-					print(chat_id)
+					# print(chat_id)
 				except:
 					pass	
 			os.remove(header+'.zip')
+			shutil.rmtree(f'{header}/')
 	print('checkout ------- '+ post.find('h3').find('a').text.strip() +' -------- ' + datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
 
 def zipdir(path, ziph):
@@ -90,60 +90,59 @@ def zipdir(path, ziph):
             ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path, '..')))
 
 def parse_week(soup):
-	# td_href = soup.find('a', {'id': 'pageTemplate__ctl3_rptrAuctions__ctl1_auctionLink'}).attrs['href']
-	# details_soup = BeautifulSoup(requests.get('http://utbod.vis.is/'+td_href).text, 'html')
-	# table = details_soup.find('table', {'id': 'carTable'})
-	# tr_list = table.find_all('tr')
-	time_now = '2021-05-10'
-	# os.mkdir(time_now)
-	# for tr in tr_list[1:]:
-	# 	tr_url = tr.find_all('td')[0].find('a').attrs['href']
-	# 	table_details = requests.get('http://utbod.vis.is'+tr_url[2:])
-	# 	tree = html.fromstring(table_details.content)
-	# 	try:
-	# 		astand = tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Damaged"]/text()')[0]
-	# 	except IndexError:
-	# 		astand = tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Damaged"]/font/text()')[0]
-	# 	car_name = tree.xpath('//*[@id="MainPage__ctl3_nameofItem"]/text()')[0]
-	# 	car_text = f'''\n
-	# 	{car_name}
-	# 	Útboðsnr.: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_number"]/text()')[0]}
-	# 	Nýskráður: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_RegDate"]/text()')[0]}
-	# 	Vélarstærð: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_EngineSize"]/text()')[0]}
-	# 	Drif: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Drive"]/text()')[0]}
-	# 	Litur: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Color"]/text()')[0]}
-	# 	Í einkaeign: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_lablePrivateOwned"]/text()')[0]}
-	# 	Ökutækjafl.: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_CarCategory"]/text()')[0]}
-	# 	Staðsetning: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Location"]/text()')[0]}
-	# 	Fastanúmer:	{tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_RegNumber"]/text()')[0]}
-	# 	Ekinn: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Driven"]/text()')[0]}
-	# 	Gírar: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Gears"]/text()')[0]}
-	# 	Hurðir: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Doors"]/text()')[0]}
-	# 	Eldsneyti: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_GasType"]/text()')[0]}
-	# 	Ástand: {astand}
-	# 	'''
-	# 	soup_car = BeautifulSoup(table_details.content, 'html')
-	# 	images_list = re.findall(r"(/items/ImageRenderer.*?)'", table_details.text)
-	# 	for img_path in range(len(images_list)):
-	# 		images_list[img_path] = images_list[img_path].replace('thumb', 'large')
-	# 		img = requests.get('http://utbod.vis.is'+images_list[img_path]).content
-	# 		with open(f'{time_now}/{car_name}-{img_path}.png', 'wb') as f:
-	# 			f.write(img)
+	td_href = soup.find('a', {'id': 'pageTemplate__ctl3_rptrAuctions__ctl1_auctionLink'}).attrs['href']
+	details_soup = BeautifulSoup(requests.get('http://utbod.vis.is/'+td_href).text, 'html')
+	table = details_soup.find('table', {'id': 'carTable'})
+	tr_list = table.find_all('tr')
+	# time_now = str(datetime.datetime.today().strftime('%Y-%m-%d'))
+	for tr in tr_list[1:]:
+		tr_url = tr.find_all('td')[0].find('a').attrs['href']
+		table_details = requests.get('http://utbod.vis.is'+tr_url[2:])
+		tree = html.fromstring(table_details.content)
+		try:
+			astand = tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Damaged"]/text()')[0]
+		except IndexError:
+			astand = tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Damaged"]/font/text()')[0]
+		car_name = tree.xpath('//*[@id="MainPage__ctl3_nameofItem"]/text()')[0]
+		car_text = f'''\n
+		{car_name}
+		Útboðsnr.: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_number"]/text()')[0]}
+		Nýskráður: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_RegDate"]/text()')[0]}
+		Vélarstærð: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_EngineSize"]/text()')[0]}
+		Drif: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Drive"]/text()')[0]}
+		Litur: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Color"]/text()')[0]}
+		Í einkaeign: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_lablePrivateOwned"]/text()')[0]}
+		Ökutækjafl.: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_CarCategory"]/text()')[0]}
+		Staðsetning: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Location"]/text()')[0]}
+		Fastanúmer:	{tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_RegNumber"]/text()')[0]}
+		Ekinn: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Driven"]/text()')[0]}
+		Gírar: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Gears"]/text()')[0]}
+		Hurðir: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_Doors"]/text()')[0]}
+		Eldsneyti: {tree.xpath('//*[@id="MainPage__ctl3_ItemDetails1_CarInfo1_GasType"]/text()')[0]}
+		Ástand: {astand}
+		'''
+		soup_car = BeautifulSoup(table_details.content, 'html')
+		images_list = re.findall(r"(/items/ImageRenderer.*?)'", table_details.text)
 
-	# 	with open(f'{time_now}/{car_name}.txt', 'w') as f:
-	# 		f.write(car_text)
-	shutil.make_archive(time_now, 'zip', time_now)
 	with open('telegram.txt', 'r') as f:
 		ids_list = f.read().split(',')
-		# with open(time_now+'.zip', 'rb') as zipObj:
+		os.mkdir(car_name)
+		for img_path in range(len(images_list)):
+			images_list[img_path] = images_list[img_path].replace('thumb', 'large')
+			img = requests.get('http://utbod.vis.is'+images_list[img_path]).content
+			with open(f'{car_name}/{car_name}-{img_path}.png', 'wb') as f:
+				f.write(img)
+			with open(f'{car_name}/{car_name}.txt', 'w') as f:
+				f.write(car_text)
+		shutil.make_archive(car_name, 'zip', car_name)
+		shutil.rmtree(f'{car_name}/')
 		for chat_id in ids_list:
 			try:
-				app.send_document(chat_id, open(time_now+'.zip','rb'))
-				print(chat_id)
+				app.send_document(chat_id, open(car_name+'.zip','rb'))
+				# print(f'{car_name} - {chat_id}')
 			except:
 				pass
-	# shutil.rmtree(f'{time_now}/')
-	os.remove(time_now+'.zip')
+		os.remove(car_name+'.zip')
 		
 def bot_thread():
 	print('### START TELEGRAM ###')
@@ -165,12 +164,12 @@ def parser_thread():
 			name = soup.find('span', {'id': 'pageTemplate__ctl3_rptrAuctions__ctl1_auctionTitle'}).text.strip()
 		except:
 			print('auction not found')
-		# try:
-		if name is not old_name:
-			parse_week(soup)
-			old_name = name
-		# except:
-		# 	print('sth went wrong in parse_week()')
+		try:
+			if name is not old_name:
+				parse_week(soup)
+				old_name = name
+		except:
+			print('sth went wrong in parse_week()')
 		time.sleep(120)
 
 if __name__ == '__main__':
