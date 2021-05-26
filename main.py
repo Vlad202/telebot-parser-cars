7cf7054cc26c7605721d621c1bbbbaec2a49ab64
Vlad202 <vladandum08@gmail.com>
2021-05-26 11:59:00 +0300

    branching a otomoto bot into a separate bot

--- main.py
+++ main.py
@@ -250,29 +250,6 @@ def fourth_parser(soup):
 	send_zip(car_name)
 	os.remove(car_name+'.zip')
 
-def fivth_parser(post_url):
-	response_post = requests.get(post_url)
-	post_soup = BeautifulSoup(response_post.text, 'html')
-	table_items = post_soup.find_all('li', {'class': 'offer-params__item'})
-	car_name = post_soup.find('span', {'class': 'offer-title'}).text.strip()
-	text = f"{post_url}\n\n{car_name}\n{post_soup.find('span', {'class': 'offer-price__number'}).text.strip()}\n\n\n"
-	car_name = 'otomoto - '+car_name
-	os.mkdir(car_name)
-	for li in table_items:
-		text += f"{li.find('span').text.strip()} - {li.find('div').text.strip()}\n\n"
-	with open(f'./{car_name}/'+car_name+'.txt', 'w') as f:
-		f.write(text)
-	slick_slides = post_soup.find_all('li', {'class': 'offer-photos-thumbs__item'})
-	for slide in range(len(slick_slides)):
-		url = slick_slides[slide].find('img').attrs['src'].split(';s=')[0]
-		image = requests.get(url).content
-		with open(f'./{car_name}/'+str(slide)+'.jpg', 'wb') as f:
-			f.write(image)
-	shutil.make_archive(car_name, 'zip', car_name)
-	shutil.rmtree(f'{car_name}/')
-	send_zip(car_name)
-	os.remove(car_name+'.zip')
-
 def bot_thread():
 	print('### START TELEGRAM ###')
 	app.polling()
@@ -282,7 +259,6 @@ def parser_thread():
 	old_name = ''
 	date_old = ''
 	old_post_name = ''
-	old_fivth = ''
 	while True:
 		# first
 		try:
@@ -345,23 +321,6 @@ def parser_thread():
 				fourth_parser(page_soup)	
 			except Exception as e:
 				print(e)
-		# fivth
-		try:
-			response_fivth = requests.get('https://www.otomoto.pl/osobowe/?search%5Bfilter_enum_damaged%5D=1&search%5Border%5D=created_at%3Adesc&search%5Bbrand_program_id%5D%5B0%5D=&search%5Bcountry%5D=')
-		except Exception as e:
-			print(e)
-			print('Exception in fivth parser')
-			continue
-		fivth_soup = BeautifulSoup(response_fivth.text, 'html')
-		first_car_fivth = fivth_soup.find_all('a', {'class': 'offer-title__link'})[0]
-		first_car_fivth_text = first_car_fivth.text.strip()
-		if first_car_fivth_text != old_fivth:
-			try:
-				print('checkout ------- otomoto ------- ' + datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
-				fivth_parser(first_car_fivth.attrs['href'])
-			except Exception as e:
-				print(e)
-			old_fivth = first_car_fivth_text
 		time.sleep(300)
 
 if __name__ == '__main__':
